f78e1c4950757cf557170af5a900d8a584fabdf5
Krishna Sai Cholkar <krishnasai3cks@gmail.com>
2020-09-13 21:06:56 +0530

    added weather api

---
+++ WeatherAPI/script.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+    let country = window.prompt("Please Enter your country", "india");
+    weatherFunction(country);
+});
+
+function weatherFunction(country) {
+    let api = `https://coronavirus-19-api.herokuapp.com/countries/${country.toLowerCase()}`;
+    $.get(api, function(res) {
+        let {
+            active,
+            cases,
+            casesPerOneMillion,
+            country,
+            critical,
+            deaths,
+            deathsPerOneMillion,
+            recovered,
+            testsPerOneMillion,
+            todayCases,
+            todayDeaths,
+            totalTests,
+        } = res;
+        $("#total-cases").html(`${cases}`);
+        $("#country").html(`${country}`);
+        var details = `${active},${cases},${casesPerOneMillion},${critical},${deathsPerOneMillion},${recovered},${deaths},${testsPerOneMillion},${todayCases},${todayDeaths},${totalTests}`.split(
+            ","
+        );
+        var ids = "active,totalcases,casesPerMillion,critical,deathsPerMillion,recovered,deaths,testsPerMillion,todayCases,todayDeaths,totalTests".split(
+            ","
+        );
+        var colorArray = [
+            "#FF6633",
+            "#ff4444",
+            "#FFB399",
+            "#FF3355",
+            "#FF3300",
+            "#33FF33",
+            "#6680B3",
+            "#66991A",
+            "#CC80CC",
+            "#66664D",
+        ];
+        for (var i in details) {
+            var a = document.createElement("div");
+            a.setAttribute("id", `details${i}`);
+            // a.setAttribute("class", "div");
+            a.innerHTML = `${ids[i].toUpperCase()} - ${details[i]}`;
+            $(a).addClass("div");
+            $(a).css({ "background-color": colorArray[i] });
+            $("body").append(a);
+        }
+    });
+}
